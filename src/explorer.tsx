0bef1177e696072b6dea50f732cfd452a22efa3a
Timothy Finucane <timfinucane@outlook.com>
2017-12-20 16:04:57 +1300

    Add explorer view

---
+++ src/explorer.tsx
@@ -0,0 +1,33 @@
+﻿/* Author: Tim Finucane, Copyright: MIT
+ * This module is for displaying the file heirarchy of the wiki,
+ * similar to other solution explorers
+ */
+
+import * as React from "react";
+
+class ElementProps
+{
+    public "data-name": string;
+}
+
+class Element extends React.Component<ElementProps, any>
+{
+    public render()
+    {
+        return <li>Element {this.props["data-name"]}</li>;
+    }
+}
+
+export class Explorer extends React.Component
+{
+    public render()
+    {
+        return (
+            <ul>
+                <Element data-name="A"/>
+                <Element data-name="B"/>
+                <Element data-name="C"/>
+            </ul>
+        );
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render( <h1>Hello World!</h1>, document.getElementById( "root" ) );
+import { Explorer } from "./explorer";
+
+ReactDOM.render( <Explorer />, document.getElementById( "root" ) );
