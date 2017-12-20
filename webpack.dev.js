2c8e82d2de075b07a39c85a8b19122b92300bea5
Timothy Finucane <timfinucane@outlook.com>
2017-12-20 15:14:40 +1300

    Update for hot module loading

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,11 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common')
+const webpack = require( 'webpack' );
+const merge = require( 'webpack-merge' )
+const common = require( './webpack.common' )
 
 module.exports = merge(
     common, 
     {
         devtool: 'inline-source-map',
-        devServer: { contentBase: './dist' }
+        devServer: { contentBase: './dist', hot: true },
+        plugins: [ new webpack.HotModuleReplacementPlugin() ]
     })
