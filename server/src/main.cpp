a5dd8ae38e9487e693fa5508dd5e8422e7c2686d
Timothy Finucane <timfinucane@outlook.com>
2018-06-20 15:00:28 +1200

    Add server side restful api setup

---
+++ server/src/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+
+#include "accounts.h"
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+
+int main(int, char**) {
+   std::cout << "Hello, world!\n";
+
+   auto file_stream = std::make_shared<concurrency::streams::ostream>();
+
+    // Open stream to output file.
+    pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
+    {
+        *file_stream = outFile;
+
+        // Create http_client to send the request.
+        http_client client(U("http://www.bing.com/"));
+
+        // Build request URI and start the request.
+        uri_builder builder(U("/search"));
+        builder.append_query(U("q"), U("cpprestsdk github"));
+        return client.request(methods::GET, builder.to_string());
+    }).then([=](http_response response)
+    {
+        printf("Received response status code: %u\n", response.status_code());
+    });
+
+
+    try
+    {
+        requestTask.wait();
+    }
+    catch (const std::exception &e)
+    {
+        printf("Error exception:%s\n", e.what());
+    }
+
+    while( true );
+}
